439d71a7052367d8789500cfef4da42f18e9d990
Kemane Wright <kemanewright@Kemanes-MacBook-Pro.local>
2018-12-30 12:49:54 -0900

    final-commit

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      DevConnector Clone - Original project and creator can be found{" "}
+      <a href="https://www.udemy.com/mern-stack-front-to-back/">Here</a>
+    </footer>
+  );
+};
